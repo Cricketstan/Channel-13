d2c4ff6a2e7c696eab4404344a04a1db110b98ca
Cricketstan <princekumarjha4518@gmail.com>
2025-09-12 02:54:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+if (window.top === window.self) {
+  window.location.href = "https://t.me/+bBTRZuMcxZ0wNGM1"; 
+}
+
+const streamUrl = "https://allinonereborn.fun/opplextv-web1/live.php?id=167583";
+
+jwplayer("my-jwplayer").setup({
+  file: streamUrl,
+  type: "hls",
+  width: "100%",
+  height: "100%",
+  autostart: true,
+  controls: true,
+  stretching: "uniform"
+});
